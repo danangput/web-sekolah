57fef34d1623862fc3bb3bd725bba6742cd90a38
susant096 <sazhunt@gmail.com>
2024-08-21 11:01:37 +0700

    routing

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 include "layout/header.php";
 include "layout/navbar.php";
-include "view/home.php";
+// include "view/home.php";
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'home';  // Default ke 'home' jika tidak ada parameter
+}
+
+switch ($page) {
+    case 'home':
+        include "view/home.php";
+        break;
+    case 'visi_misi':
+        include "view/visi_misi.php";
+        break;
+
+}
+
 include "layout/footer.php";
+
+//penggunaan routing http://localhost/web-sekolah/index.php?page=visi_misi
 ?>
