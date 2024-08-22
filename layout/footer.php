2bd0e49505d256c93cdacfb6263463c4e72cf14d
Fiernaz Ingga <inggapratama86@gmail.com>
2024-08-22 16:49:22 +0700

    untuk jurusan dan detail jurusan

--- layout/footer.php
+++ layout/footer.php
@@ -1,7 +1,7 @@
 <footer>
       <div class="container-fluid bg-success-subtle text-dark  text-center pb-1 pt-3">
         <p class="p">
-          Copyright &copy; August 2024 | powered by: Danang & Rustam
+          Copyright &copy; August 2024 | powered by: Danang & Rustam & Fiernaz
         </p>
       </div>
     </footer>
--- view/jurusan.php
+++ view/jurusan.php
@@ -1,62 +1,77 @@
-
 <section>
-  <div class="container">
-    <div class="row d-flex justify-content-center">
-      <div class="col-10">
-        <img src="public/img/jurusanbg.jpg" class="img-fluid mb-5" alt="...">
-      </div>
-    </div>
     <div class="container">
-  <div class="row">
-    <div class="row">
-      <h2 class="h2">Informatika</h2>
-      <div class="container">
-  <div class="row">
-    <div class="col">
-      <img src="public/img/coding.png" alt="" class="rounded float-start" style="width: 50%">
-    </div>
-    <div class="col">
-      <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, minima. Neque quibusdam sint
-      animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci accusantium pariatur beatae
-      doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur animi et ipsum, tempore libero
-      id nemo magnam dolores fuga eos ex dolore delectus!
-            </p>    
-      </div>
-    </div>
-    </div>
-    <div class="row">
-    <h2 class="h2">Machine Learning</h2>
-    <div class="container">
-  <div class="row">
-    <div class="col">
-    <img src="public/img/ml.jpg" alt="" class="rounded float-start" style="width: 50%">
-    </div>
-    <div class="col">
-    <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, minima. Neque quibusdam sint
-            animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci accusantium pariatur beatae
-            doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur animi et ipsum, tempore libero
-            id nemo magnam dolores fuga eos ex dolore delectus!</p>
-    </div>
-</div>
-            
-          
-    </div>
-    <div class="row">
-    <h2 class="h2">Data Analyst</h2>
-    <div class="row">
-    <div class="col">
-    <img src="public/img/analis.jpg" alt="" class="rounded float-start" style="width: 50%">
-    </div>
-    <div class="col">
-    <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, minima. Neque quibusdam sint
-            animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci accusantium pariatur beatae
-            doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur animi et ipsum, tempore libero
-            id nemo magnam dolores fuga eos ex dolore delectus!</p>
-    </div>
-</div>
-            
-          
-    </div>
-  </div>
-</div>
-  </section>
+        <div class="row d-flex justify-content-center">
+            <div class="col-10">
+                <img src="public/img/jurusanbg.jpg" class="img-fluid mb-5" alt="...">
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="row">
+
+                    <!-- gambar pertama -->
+
+                    <h2 class="h2">Informatika</h2>
+                    <div class="grid text-center">
+                        <div class="g-col-6">
+                            <img src="public/img/coding.png" alt="" class="rounded float-start" style="width: 50%">
+                        </div>
+                        <div class="g-col-6">
+                            <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, minima.
+                                Neque quibusdam sint
+                                animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci
+                                accusantium pariatur beatae
+                                doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur animi et
+                                ipsum, tempore libero
+                                id nemo magnam dolores fuga eos ex dolore delectus!
+                            </p>
+                            <a href="/web-sekolah/index.php?page=infor"><button type="button"
+                                    class="btn btn-outline-primary">Lihat Detail Jurusan</button></a>
+                        </div>
+                    </div>
+
+                    <!-- gambar kedua -->
+
+                    <h2 class="h2">Machine Learning</h2>
+                    <div class="grid text-center">
+                        <div class="g-col-6">
+                            <img src="public/img/ml.jpg" alt="" class="rounded float-start" style="width: 50%">
+                        </div>
+                        <div class="g-col-6">
+                            <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+                                minima. Neque quibusdam sint
+                                animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci
+                                accusantium pariatur bea
+                                doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur
+                                animi et ipsum, tempore libero
+                                id nemo magnam dolores fuga eos ex dolore delectus!
+                            </p>
+                            <a href="/web-sekolah/index.php?page=machine"><button type="button"
+                                    class="btn btn-outline-primary">Lihat Detail Jurusan</button></a>
+                        </div>
+                    </div>
+
+                    <!-- gambar ketiga -->
+
+                    <h2 class="h2">Data Analyst</h2>
+                    <div class="grid text-center">
+                        <div class="g-col-6">
+                            <img src="public/img/analis.jpg" alt="" class="rounded float-start" style="width: 50%">
+                        </div>
+                        <div class="g-col-6">
+                            <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis,
+                                minima. Neque quibusdam sint
+                                animi amet ex possimus. Quam nemo ab necessitatibus, perspiciatis animi adipisci
+                                accusantium pariatur beatae
+                                doloremque? Temporibus nobis doloremque eveniet qui iure ipsum! Consequatur
+                                animi et ipsum, tempore libero
+                                id nemo magnam dolores fuga eos ex dolore delectus!
+                            </p>
+                            <a href="/web-sekolah/index.php?page=analis"><button type="button" class="btn btn-outline-primary">Lihat Detail
+                                    Jurusan</button></a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+</section>
