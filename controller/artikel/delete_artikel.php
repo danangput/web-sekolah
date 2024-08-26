6ececd85189c2e8e11563613bd46a530638bf136
Danang <danangput10@gmail.com>
2024-08-26 10:23:02 +0700

    hapus

--- controller/artikel/delete_artikel.php
+++ controller/artikel/delete_artikel.php
@@ -1,9 +1,9 @@
 <?php 
-include "../db_connect.php";
-
-$id = $_GET['id'];
-$query = "DELETE FROM artikel WHERE id = $id";
+include __DIR__. "\..\db_connect.php";
+$id="";
+$id = $_GET['id_artikel'];
+$query = "DELETE FROM artikel WHERE id_artikel = $id";
 $result = mysqli_query($conn, $query);
-header("Location: admin.php?page=artikel");
+header("location: ".$_SERVER['PHP_SELF']."?refreshed=1");
 
 ?>
