c0d26d29c8a47751f38c9fb6ce7bda4c509f1b7c
Fiernaz Ingga <inggapratama86@gmail.com>
2024-08-26 15:54:16 +0700

    tes

--- layout/navbar.php
+++ layout/navbar.php
@@ -1,31 +1,32 @@
-
 <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid fixed-top"  style="background-color: #e3f2fd; margin-bottom:300px;">
-      <a class="navbar-brand" href="#">
-        <img src="public/img/logo.png" alt="Bootstrap" width="70" height="70">
-      </a>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav mx-auto">
-          <li class="nav-item">
-            <a class="nav-link active fw-bold " aria-current="page" href="/web-sekolah/index.php?page=home">HOME</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-bold" href="http://localhost/web-sekolah/index.php?page=visi_misi">VISI DAN MISI</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=jurusan">JURUSAN</a>
-          </li>
-          <li>
-          <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=prestasi">PRESTASI</a>
-          </li>
-          <li>
-          <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=artikel">ARTIKEL</a>
-          </li>
-          <!-- <form action="login.php" method="POST">
+    <div class="container-fluid fixed-top" style="background-color: #e3f2fd; margin-bottom:300px;">
+        <a class="navbar-brand" href="#">
+            <img src="public/img/logo.png" alt="Bootstrap" width="70" height="70">
+        </a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link active fw-bold " aria-current="page"
+                        href="/web-sekolah/index.php?page=home">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="http://localhost/web-sekolah/index.php?page=visi_misi">VISI DAN
+                        MISI</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=jurusan">JURUSAN</a>
+                </li>
+                <li>
+                    <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=prestasi">PRESTASI</a>
+                </li>
+                <li>
+                    <a class="nav-link fw-bold" href="/web-sekolah/index.php?page=artikel">ARTIKEL</a>
+                </li>
+                <!-- <form action="login.php" method="POST">
             <button type="submit" name="Logout" class="btn btn-outline-primary">Logout</button>
           </form> -->
-        </ul>
-      </div>
+            </ul>
+        </div>
     </div>
-  </div>
-  </nav>
+    </div>
+</nav>
