c96e8a7cf6cea459d5559cb3d8146a9178b5ac82
susant096 <sazhunt@gmail.com>
2024-08-28 14:38:30 +0700

    tanggal indo

--- view/show_artikel.php
+++ view/show_artikel.php
@@ -5,11 +5,14 @@
         <header class="article-header mb-3">
             <img src="public/img/artikel/<?= $detail_artikel['gambar'] ?>" class="img-fluid" alt="Gambar Artikel">
             <h1 class="mt-3"><?= $detail_artikel['judul_artikel'] ?></h1>
-            <p class="text-muted"><?= $detail_artikel['tanggal'] ?></p>
+            <p class="text-muted"><?php
+            $date = new DateTime($detail_artikel['tanggal']);
+            $formatter = new IntlDateFormatter('id_ID', IntlDateFormatter::FULL, IntlDateFormatter::NONE);
+            echo $formatter->format($date);
+            ?></p>
         </header>
-
         <div class="article-content">
-            <p><?= $detail_artikel['deskripsi'] ?> </p>
+            <p class="" style="text-align: justify"><?= $detail_artikel['deskripsi'] ?> </p>
         </div>
     </article>
 </div>
