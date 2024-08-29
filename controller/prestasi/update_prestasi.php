3bc665a4b73a4305eb4baba831ffbc7d2f31f9b8
Fiernaz Ingga <inggapratama86@gmail.com>
2024-08-29 21:57:45 +0700

    update fiks complite

--- controller/prestasi/update_prestasi.php
+++ controller/prestasi/update_prestasi.php
@@ -14,13 +14,18 @@
   $result = mysqli_fetch_assoc($query_prestasi);
   $old_gambar = $folder . $result['gambar_prestasi'];
   
+// Jika gambar baru di-upload
+if ($gambar) {
+  // Hapus gambar lama jika ada
   if (file_exists($old_gambar)) {
-    if (!$gambar) {
-      $gambar = $result['gambar_prestasi'];
-    }
     unlink($old_gambar);
-    move_uploaded_file($newTmpName, $folder . $gambar);
   }
+  // Pindahkan gambar baru ke folder
+  move_uploaded_file($newTmpName, $folder . $gambar);
+} else {
+  // Jika tidak ada gambar baru, gunakan gambar lama
+  $gambar = $result['gambar_prestasi'];
+}
 
   $query = "UPDATE prestasi SET nama = '$nama', deskripsi = '$deskripsi', gambar_prestasi = '$gambar', tanggal = '$tanggal' WHERE id_prestasi = $id";
 
