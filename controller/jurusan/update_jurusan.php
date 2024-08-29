5ec02adcbda8fbde2e3115017fb29f57e9c5f10b
Fiernaz Ingga <inggapratama86@gmail.com>
2024-08-29 21:49:29 +0700

    update complite

--- controller/jurusan/update_jurusan.php
+++ controller/jurusan/update_jurusan.php
@@ -3,8 +3,27 @@
 
   $id = $_POST['id_jurusan'];
   $nama = $_POST['nama_jurusan'];
-  $gambar = "gambar";
   $deskripsi = $_POST['deskripsi'];
+  $gambar = $_FILES['gambar-jurusan']['name'];
+  $newTmpImg = $_FILES['gambar-jurusan']['tmp_name'];
+  $folder = 'public/img/jurusan/';
+
+  $query_jurusan = mysqli_query($conn, "SELECT * FROM jurusan WHERE id_jurusan = $id");
+  $result = mysqli_fetch_assoc($query_jurusan);
+  $old_gambar = $folder . $result['gambar'];
+
+// Jika gambar baru di-upload
+if ($gambar) {
+  // Hapus gambar lama jika ada
+  if (file_exists($old_gambar)) {
+    unlink($old_gambar);
+  }
+  // Pindahkan gambar baru ke folder
+  move_uploaded_file($newTmpImg, $folder . $gambar);
+} else {
+  // Jika tidak ada gambar baru, gunakan gambar lama
+  $gambar = $result['gambar'];
+}
 
   $query = "UPDATE jurusan SET nama_jurusan = '$nama', gambar = '$gambar', deskripsi = '$deskripsi' WHERE id_jurusan = $id";
 
