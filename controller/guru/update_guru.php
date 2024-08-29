e21b23ea283228edd6bd848afa0079aa9fae41f8
susant096 <sazhunt@gmail.com>
2024-08-29 14:32:01 +0700

    guru upload logic

--- controller/guru/update_guru.php
+++ controller/guru/update_guru.php
@@ -5,23 +5,23 @@ $id = $_POST['id_guru'];
 $nama = $_POST['nama_guru'];
 $alamat = $_POST['alamat'];
 $no_telp = $_POST['no_telp'];
-$image = $_FILES['gambar'];
+$image = $_FILES['gambar']['name'];
+$newTmpName = $_FILES['gambar']['tmp_name'];
 // var_dump($image);
 // die();
 $query_guru = mysqli_query($conn, "SELECT * FROM guru WHERE id_guru = $id");
 $result = mysqli_fetch_assoc($query_guru);
-$path_gambar = 'public/img/guru/' . $result['gambar_guru'];
+$old_gambar = 'public/img/guru/' . $result['gambar_guru'];
 
-if (file_exists($path_gambar)) {
-  echo "File already exists";
-  exit();
-} else {
-  echo "File does not exist";
+if (file_exists($old_gambar)) {
+  if (!$image) {
+    $image = $result['gambar_guru'];
+  }
+  // hapus gambar Lama
+  // unlink();
+  // upload gambar baru
+  // move_uploaded_file();
 }
-die();
-// $image = $_FILES['image']['name'];
-// $tmpname = $_FILES['image']['tmp_name'];
-// move_uploaded_file($tmpname, $folder);
 
 $query = "UPDATE guru SET nama_guru = '$nama', alamat= '$alamat', no_telp = '$no_telp', gambar_guru= '$image' WHERE id_guru = $id";
 
