e4b3d0a853ebd84b850ffa829b0fe55279d11ccc
MRAcodeDEVcici <ikbnprogramming@gmail.com>
2024-08-29 20:02:00 +0700

    ralat gambar title

--- layout/header.php
+++ layout/header.php
@@ -7,7 +7,7 @@
   <title>Web Sekolah</title>
   <link rel="stylesheet" href="assets/boostrap/css/bootstrap.min.css" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="shortcut icon" href="public/img/logo-fav-home.png" type="image/x-icon">
+  <link rel="shortcut icon" href="public/img/logo-fav-sekolah.png" type="image/x-icon">
   <style>
     body {
       background-color: #fcfcfc;
