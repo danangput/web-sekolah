443aabbe07b6aaf2679659022b4431938b8e68bd
MRAcodeDEVcici <ikbnprogramming@gmail.com>
2024-08-29 21:38:21 +0700

    Added "table-hover" class to table element in table.php.

--- client/table.php
+++ client/table.php
@@ -16,7 +16,7 @@
                     Tambah</a> -->
             </div>
             <div class="table-responsive">
-                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>No</th>
