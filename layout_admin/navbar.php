a39157b3a21df27fa7b72e2de876146d7cc7dfe1
MRAcodeDEVcici <ikbnprogramming@gmail.com>
2024-08-29 23:41:40 +0700

    memperbarui

--- layout_admin/navbar.php
+++ layout_admin/navbar.php
@@ -1,5 +1,5 @@
  <!-- Sidebar -->
- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar" style="letter-spacing: 2px">
+ <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="letter-spacing: 2px; background: linear-gradient(to top, #071952, black)">
 
      <!-- Sidebar - Brand -->
      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -16,7 +16,7 @@
      <li class="nav-item active">
          <a class="nav-link" href="admin.php?page=dashboard">
              <i class="fas fa-tachometer-alt"></i>
-             <span>Dashboard</span></a>
+             <span class="text-uppercase">Dashboard</span></a>
      </li>
 
      <hr class="sidebar-divider my-0">
@@ -24,35 +24,35 @@
      <li class="nav-item">
          <a class="nav-link" href="admin.php?page=artikel">
              <i class="fas fa-book-open"></i>
-             <span>Artikel</span>
+             <span class="text-uppercase">Artikel</span>
          </a>
      </li>
 
      <li class="nav-item">
          <a class="nav-link" href="admin.php?page=guru">
              <i class="fas fa-chalkboard-teacher"></i>
-             <span>Guru</span>
+             <span class="text-uppercase">Guru</span>
          </a>
      </li>
 
      <li class="nav-item">
          <a class="nav-link" href="admin.php?page=jurusan">
              <i class="fas fa-university"></i>
-             <span>Jurusan</span>
+             <span class="text-uppercase">Jurusan</span>
          </a>
      </li>
 
      <li class="nav-item">
          <a class="nav-link" href="admin.php?page=prestasi">
              <i class="fas fa-trophy"></i>
-             <span>Prestasi</span>
+             <span class="text-uppercase">Prestasi</span>
          </a>
      </li>
 
      <li class="nav-item">
          <a class="nav-link" href="admin.php?page=client">
              <i class="fas fa-pray"></i>
-             <span>Client</span>
+             <span class="text-uppercase">Client</span>
          </a>
      </li>
 
--- login.php
+++ login.php
@@ -24,7 +24,7 @@ if (isset($_POST['Login'])) {
         $_SESSION['username'] = $data['username'];
         $_SESSION['isLogin'] = true;
 
-        header("location: admin.php");
+        header("location: admin.php?page=dashboard");
     } else {
         $loginMessage = true;
     }
@@ -42,6 +42,7 @@ if (isset($_POST['Login'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="shortcut icon" href="public/img/logo-fav.png" type="image/x-icon">
 </head>
 
 <body style="background-image: url('public/img/bgLogin.jpeg'); background-size: cover; background-repeat: no-repeat;">
@@ -87,6 +88,6 @@ if (isset($_POST['Login'])) {
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+</script>
 
 </html>
