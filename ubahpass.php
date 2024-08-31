e6fff68e3115717084f8fd5dd8badb12e168c7b1
MRAcodeDEVcici <ikbnprogramming@gmail.com>
2024-08-31 11:43:34 +0700

    Added Font Awesome CSS link and spinner icon to "Change password" button.

--- ubahpass.php
+++ ubahpass.php
@@ -39,6 +39,7 @@ if (isset($_POST['ubah'])) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="shortcut icon" href="public/img/logo-fav.png" type="image/x-icon">
 </head>
 
@@ -70,7 +71,7 @@ if (isset($_POST['ubah'])) {
                         <input type="password" class="form-control" id="inputPassword4" name="newpass" required>
                     </div>
                     <div class="col-12 d-flex justify-content-center">
-                        <button type="submit" class="btn btn-primary" name="ubah">Change password</button>
+                        <button type="submit" class="btn btn-primary" name="ubah"><i class="fas fa-spinner"></i> Change password</button>
                     </div>
                 </form>
             </div>
